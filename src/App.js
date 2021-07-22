2a8a4f02e1ff4f633b1a3341ae1e56d847024378
autoex <autoex@gmail.com>
2021-07-22 13:57:23 +0300

    all functionality in one file without redux and components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.css'
+import {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    const [input, setInput] = useState('');
+    const [todos, setTodos] = useState([]);
+    const inputHandler = (txt) => {
+        setInput(txt);
+    };
+
+    const addTodo = () => {
+        if (!input.trim().length) return;
+        setTodos([
+            ...todos, {
+                id: new Date().valueOf(),
+                completed: false,
+                txt: input
+            }
+        ]);
+        setInput('')
+
+    };
+    const removeTodo = (id) => {
+        setTodos(
+            todos.filter(todo => todo.id !== id)
+        )
+
+
+    };
+
+    const checkedToggle = (id) => {
+       setTodos(
+           todos.map(todo=> {
+               if( todo.id !== id) return todo;
+               return {...todo,
+               completed: !todo.completed}
+           })
+       )
+
+    };
+    return (
+        <div className='app'>
+            <div className='inputForm'>
+                <input type="text" placeholder={'Your to do..'} value={input}
+                       onChange={event => inputHandler(event.target.value)}/>
+                <button onClick={addTodo}>Add</button>
+            </div>
+            <div className="todoList">
+                <ul>
+                    {todos.map(todo =>
+                        <li key={todo.id}>
+                            <input type="checkbox" checked={todo.completed} onChange={()=>checkedToggle(todo.id)}/>
+                            <span>{todo.txt}</span>
+                            <span onClick={() => removeTodo(todo.id)}>&times;</span>
+                        </li>)}
+                </ul>
+            </div>
+
+        </div>
+    );
+};
 
 export default App;
